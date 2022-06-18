781e2d395762658d4227e76ebea685d9b4d09c98
Diseyi <107358426+deeseyi@users.noreply.github.com>
2022-06-18 19:42:40 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.url === '/') {
+
+        fs.createReadStream('./client/index.html').pipe(res)
+    }
+    if (req.url === '/about') {
+
+        fs.createReadStream('./client/about.html').pipe(res)
+    }
+    if (req.url === '/contact') {
+
+        fs.createReadStream('./client/contact.html').pipe(res)
+    }
+  
+});
+
+
+server.listen(1337, () => {
+    console.log('Server is listening on port 1337');
+});
+
+
+
+
+
+
